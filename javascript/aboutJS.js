2f2a4002f77eb3f8e8bc1425b19a7b583732192c
berout <123272384+berout@users.noreply.github.com>
2023-09-22 18:09:35 +0200

    Update aboutJS.js

--- javascript/aboutJS.js
+++ javascript/aboutJS.js
@@ -1,46 +1,46 @@
 // EYES WIDE OPEN
 
+// function parallax(e){
+//     if (window.matchMedia("(max-width: 830px)").matches){
+//         document.addEventListener("deviceorientation", parallax);
+//         this.querySelectorAll('.layer').forEach(layer =>{
+//             const x = e.beta;
+//             const y = e.gamma;
+//             const z = e.alpha;
+//             layer.style.transform = `translateX(-${x}px%) translateY(-${y}px%) translateZ(-${z}px%)`});
+//     } else{
+//         document.addEventListener("mousemove", parallax);
+//         this.querySelectorAll('.layer').forEach(layer =>{
+//             const speed = layer.getAttribute('data-speed');
+//             const x = (window.innerWidth - e.pageX * speed)/100;
+//             const y = (window.innerHeight - e.pageY * speed)/100;
+//             layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
+//     });
+//     };
+// };
+
+
+
+
+document.addEventListener("mousemove", parallax);
 function parallax(e){
-    if (window.matchMedia("(max-width: 830px)").matches){
-        document.addEventListener("deviceorientation", parallax);
-        this.querySelectorAll('.layer').forEach(layer =>{
-            const x = e.beta;
-            const y = e.gamma;
-            const z = e.alpha;
-            layer.style.transform = `translateX(-${x}px%) translateY(-${y}px%) translateZ(-${z}px%)`});
-    } else{
-        document.addEventListener("mousemove", parallax);
-        this.querySelectorAll('.layer').forEach(layer =>{
-            const speed = layer.getAttribute('data-speed');
-            const x = (window.innerWidth - e.pageX * speed)/100;
-            const y = (window.innerHeight - e.pageY * speed)/100;
-            layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
-    });
-    };
+    this.querySelectorAll('.layer').forEach(layer =>{
+        const speed = layer.getAttribute('data-speed');
+        const x = (window.innerWidth - e.pageX * speed)/100;
+        const y = (window.innerHeight - e.pageY * speed)/100;
+        layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
+    })
 };
 
-
-
-
-// document.addEventListener("mousemove", parallax);
-// function parallax(e){
-//     this.querySelectorAll('.layer').forEach(layer =>{
-//         const speed = layer.getAttribute('data-speed');
-//         const x = (window.innerWidth - e.pageX * speed)/100;
-//         const y = (window.innerHeight - e.pageY * speed)/100;
-//         layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
-//     })
-// };
-
-// document.addEventListener("deviceorientation", parallax);
-// function parallax(e){
-//     this.querySelectorAll('.layer').forEach(layer =>{
-//         const x = e.beta;
-//         const y = e.gamma;
-//         const z = e.alpha;
-//         layer.style.transform = `translateX(-${x}px%) translateY(-${y}px%) translateZ(-${z}px%)`;
-//     })
-// };
+document.addEventListener("deviceorientation", parallax);
+function parallax(e){
+    this.querySelectorAll('.layer').forEach(layer =>{
+        const x = e.beta;
+        const y = e.gamma;
+        const z = e.alpha;
+        layer.style.transform = `translateX(-${x}px%) translateY(-${y}px%) translateZ(-${z}px%)`;
+    })
+};
 
 
 
