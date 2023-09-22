cbcc0e42db71246623600558e0a3af9215f8a0de
berout <123272384+berout@users.noreply.github.com>
2023-09-22 14:07:24 +0200

    Create portfolioJS.js

---
+++ portfolioJS.js
@@ -0,0 +1,8 @@
+document.addEventListener('mousemove', function(e){
+    const body = document.querySelector('body');
+    const bottom = document.createElement('span');
+    bottom.style.left= -75 + e.offsetX+'px';
+    bottom.style.top= -75 + e.offsetY+'px';
+    body.appendChild(bottom);
+});
+
