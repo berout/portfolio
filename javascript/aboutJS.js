10deba953a1fffc95dddf59a369c0c6ef646565b
berout <123272384+berout@users.noreply.github.com>
2023-09-22 15:29:50 +0200

    Update aboutJS.js

--- javascript/aboutJS.js
+++ javascript/aboutJS.js
@@ -1,23 +1,45 @@
 // EYES WIDE OPEN
-document.addEventListener("mousemove", parallax);
-function parallax(e){
-    this.querySelectorAll('.layer').forEach(layer =>{
-        const speed = layer.getAttribute('data-speed');
-        const x = (window.innerWidth - e.pageX * speed)/100;
-        const y = (window.innerHeight - e.pageY * speed)/100;
-        layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
-    })
-};
 
-document.addEventListener("deviceorientation", parallax);
 function parallax(e){
-    this.querySelectorAll('.layer').forEach(layer =>{
-        const x = e.beta;
-        const y = e.gamma;
-        const z = e.alpha;
-        layer.style.transform = `translateX(-${x}px%) translateY(-${y}px%) translateZ(-${z}px%)`;
-    })
-};
+    if (window.matchMedia("(max-width: 830px)"){
+        document.addEventListener("deviceorientation", parallax);
+        this.querySelectorAll('.layer').forEach(layer =>{
+            const x = e.beta;
+            const y = e.gamma;
+            const z = e.alpha;
+            layer.style.transform = `translateX(-${x}px%) translateY(-${y}px%) translateZ(-${z}px%)`});
+    } else{
+        document.addEventListener("mousemove", parallax);
+        this.querySelectorAll('.layer').forEach(layer =>{
+            const speed = layer.getAttribute('data-speed');
+            const x = (window.innerWidth - e.pageX * speed)/100;
+            const y = (window.innerHeight - e.pageY * speed)/100;
+            layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
+    });
+    };
+
+
+
+
+// document.addEventListener("mousemove", parallax);
+// function parallax(e){
+//     this.querySelectorAll('.layer').forEach(layer =>{
+//         const speed = layer.getAttribute('data-speed');
+//         const x = (window.innerWidth - e.pageX * speed)/100;
+//         const y = (window.innerHeight - e.pageY * speed)/100;
+//         layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
+//     })
+// };
+
+// document.addEventListener("deviceorientation", parallax);
+// function parallax(e){
+//     this.querySelectorAll('.layer').forEach(layer =>{
+//         const x = e.beta;
+//         const y = e.gamma;
+//         const z = e.alpha;
+//         layer.style.transform = `translateX(-${x}px%) translateY(-${y}px%) translateZ(-${z}px%)`;
+//     })
+// };
 
 
 
