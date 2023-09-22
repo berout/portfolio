56000e76eb73537571f78e3db4ba92191ea4c2bd
berout <123272384+berout@users.noreply.github.com>
2023-09-22 15:36:24 +0200

    Update aboutJS.js

--- javascript/aboutJS.js
+++ javascript/aboutJS.js
@@ -1,7 +1,7 @@
 // EYES WIDE OPEN
 
 function parallax(e){
-    if (window.matchMedia("(max-width: 830px)")){
+    if (window.matchMedia("(max-width: 830px)").matches){
         document.addEventListener("deviceorientation", parallax);
         this.querySelectorAll('.layer').forEach(layer =>{
             const x = e.beta;
