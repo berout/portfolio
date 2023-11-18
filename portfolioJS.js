1d80bf0ae2c071058d0c5094feb0e3abd50bd038
berout <berutmagda@gmail.com>
2023-11-18 01:59:15 +0100

    updating website

---
+++ portfolioJS.js
@@ -0,0 +1,9 @@
+// PAINTING
+document.addEventListener('mousemove', function(e){
+    const body = document.querySelector('body');
+    const bottom = document.createElement('span');
+    bottom.style.left= -75 + e.offsetX+'px';
+    bottom.style.top= -75 + e.offsetY+'px';
+    body.appendChild(bottom);
+});
+
