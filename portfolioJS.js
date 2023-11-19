0541356a2901c0ab0af7951e03854d5dd80dcfc1
berout <berutmagda@gmail.com>
2023-11-20 00:17:34 +0100

    changes added

--- portfolioJS.js
+++ portfolioJS.js
@@ -1,9 +1,10 @@
 // PAINTING
 document.addEventListener('mousemove', function(e){
-    const body = document.querySelector('body');
+    const main = document.querySelector('main');
     const bottom = document.createElement('span');
+    const top = document.querySelector('.top');
     bottom.style.left= -75 + e.offsetX+'px';
     bottom.style.top= -75 + e.offsetY+'px';
-    body.appendChild(bottom);
-});
+    main.appendChild(bottom);
 
+});
