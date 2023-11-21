994fea64d7cbc04ab0df37fcbe6fc202f48dd157
berout <berutmagda@gmail.com>
2023-11-21 18:42:30 +0100

    painting bug resolved

--- portfolioJS.js
+++ portfolioJS.js
@@ -2,9 +2,7 @@
 document.addEventListener('mousemove', function(e){
     const main = document.querySelector('main');
     const bottom = document.createElement('span');
-    const top = document.querySelector('.top');
-    bottom.style.left= -75 + e.offsetX+'px';
-    bottom.style.top= -75 + e.offsetY+'px';
+    bottom.style.left= -75 + e.clientX + "px";
+    bottom.style.top= -75 + e.clientY + "px";
     main.appendChild(bottom);
-
 });
